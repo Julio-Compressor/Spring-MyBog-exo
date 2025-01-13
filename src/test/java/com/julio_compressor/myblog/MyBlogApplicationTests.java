f90a47afddffee4d33c92a744956e19430b33c73
julien leblanc <julien2.leblanc@gmail.com>
2025-01-13 10:54:42 +0100

    bootstrap project

---
+++ src/test/java/com/julio_compressor/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio_compressor.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
